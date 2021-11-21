5c757583f12e2bd9a592d1b381fecbd2eafc7494
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-11-21 03:16:21 -0600

    LC0001 solution

--- LeetCode/Solutions/LC0001.py
+++ LeetCode/Solutions/LC0001.py
@@ -1,3 +1,17 @@
+'''
+Solution
+
+There isn't too much I can add to the discussion of this classic leetcode question.
+In order to get two numbers that sum to the target we must add the current number with target - num
+So for each number we store it's complement in the dictionary and if we come across a number that has
+been added to the dictionary we know that it's complement must also have been seen before.
+
+Complexity
+
+Time - O(n): We iterate over each number at least once
+Space - O(n): The dictionary stores at most n elements (where n is the length of nums)
+
+'''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         differences = {}
