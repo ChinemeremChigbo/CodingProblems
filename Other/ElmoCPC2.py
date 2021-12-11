3e804d81a1a615486f75725f314df80f9f936619
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-12-11 14:33:02 -0600

    Q2

--- Other/ElmoCPC2.py
+++ Other/ElmoCPC2.py
@@ -1,23 +1,37 @@
-# import SortedDict
+from collections import defaultdict
+def helper(n,k,l):
+    ls = sorted(l)
+    lDict = defaultdict(set)
+    a = 1
+    for i,num in enumerate(ls):
+        if i and i%k == 0:
+            a += 1
+        lDict[a].add(num)
+    a = 1
+    pointer = 0
+    popped = 0
+    res = 0
+    while popped < n - k:
+        stop = True
+        if popped and popped%k == 0:
+            a += 1
+        if l[pointer] in lDict[a]:
+            if pointer != 0:
+                res += 1
+            l.pop(pointer)
+            popped += 1
+            pointer -= 1
+            stop = False
+        pointer += 1
+        if pointer == len(l):
+            if stop:
+                break
+            else:
+                pointer = 0
+    return res
+    
 
-from collections import deque
 
-def helper(l1,l2):
-    l1.sort()
-    l2.sort()
-    print(l1,l2)
-    for i,name in enumerate(l2):
-        if l1[i] != name:
-            return l1[i]
-    return l1[-1]
-
-n = int(input())
-l1 = []
-l2 = []
-for i in range(n + n - 1):
-    p = input()
-    if i < n:
-        l1.append(p)
-    else:
-        l2.append(p)
-print(helper(l1,l2))
+n, k = [int(i) for i in input().split()]
+l = [int(i) for i in input().split()]
+print(helper(n,k,l))
