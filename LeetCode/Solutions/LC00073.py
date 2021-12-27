d365fd69a119b0cacc9f168f3b324171d6deff98
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-12-27 02:20:05 -0600

    Update LC00073.py

---
+++ LeetCode/Solutions/LC00073.py
@@ -0,0 +1,17 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rowSet = set()
+        colSet = set()
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    rowSet.add(i)
+                    colSet.add(j)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i in rowSet or j in colSet:
+                    matrix[i][j] = 0
