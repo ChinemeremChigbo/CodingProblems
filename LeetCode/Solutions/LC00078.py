6853d34cb1ddaad3d4f04745fade51b91537c6fd
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-12-27 02:22:11 -0600

    Update LC00078.py

---
+++ LeetCode/Solutions/LC00078.py
@@ -0,0 +1,8 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        finalList = []
+        for i in range(2**len(nums)):
+            listEntry = sorted([nums[j] for j in range(len(nums)) if ('{:0>10}'.format(str(bin(i))[2:])[::-1][j]) == "1" ])
+            if listEntry not in finalList:
+                finalList += [listEntry]
+        return finalList
