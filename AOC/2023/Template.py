a477bb1da85ef36e67b2c7fd9fce942a21203b7e
ChinemeremChigbo <chinemeremchigbo@Outlook.com>
2023-12-03 13:33:57 +0800

    Add Template.py file

---
+++ AOC/2023/Template.py
@@ -0,0 +1,22 @@
+def star_one():
+    file = open("Day0.txt", "r")
+    res = 0
+
+    for idx, line in enumerate(file):
+        pass
+
+    print(res)
+
+
+def star_two():
+    file = open("Day0.txt", "r")
+    res = 0
+
+    for idx, line in enumerate(file):
+        pass
+
+    print(res)
+
+
+star_one()
+star_two()
